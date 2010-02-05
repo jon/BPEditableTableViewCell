1bff735bbf76e7a8893385a7ec4b35883227f72d
Jon Olson <jon@damogran.com>
2010-02-05 18:03:10 -0500

    Added missing superclass includes

--- BPSwitchEditableTableViewCell.h
+++ BPSwitchEditableTableViewCell.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "BPEditableTableViewCell.h"
 
 @interface BPSwitchEditableTableViewCell : BPEditableTableViewCell {
 
--- BPTextFieldEditableTableViewCell.h
+++ BPTextFieldEditableTableViewCell.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "BPEditableTableViewCell.h"
 
 @interface BPTextFieldEditableTableViewCell : BPEditableTableViewCell {
 
